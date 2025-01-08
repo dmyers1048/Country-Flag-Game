4f1c4c71f1019277f551a76c6bbed625002f3032
dmyers1048 <dmyers1@bsd220.org>
2025-01-08 09:31:01 -0600

    Initial Commit

---
+++ Country Flag Game/Country_Flag_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Country_Flag_GameApp.swift
+//  Country Flag Game
+//
+//  Created by Devan Myers on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Country_Flag_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
