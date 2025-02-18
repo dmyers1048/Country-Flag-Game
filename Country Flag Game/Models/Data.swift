da5c28ee25ba973a7def239462019f73a1ffab59
dmyers1048 <dmyers1@bsd220.org>
2025-02-18 09:25:29 -0600

    Addded Countrys

---
+++ Country Flag Game/Models/Data.swift
@@ -0,0 +1,33 @@
+//
+//  Data.swift
+//  Country Flag Game
+//
+//  Created by Devan Myers on 1/8/25.
+//
+
+import SwiftUI
+
+struct Data {
+    let countries = [
+        "Angola",
+        "Austria",
+        "China",
+        "England",
+        "Estonia",
+        "France",
+        "Germany",
+        "Greece",
+        "India",
+        "Isreal",
+        "Italy",
+        "Norway",
+        "Pakistan",
+        "Peru",
+        "Saudi Arabia",
+        "South Africa",
+        "Spain",
+        "Sweden",
+        "Ukraine",
+        "United States"
+    ]
+}
--- Country Flag Game/Views/ContentView.swift
+++ Country Flag Game/Views/ContentView.swift
@@ -8,14 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var gameManager = GameManager()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack(spacing: 40) {
+                VStack(spacing: 30) {
+                    Text("CFG")
+                        .font(.title)
+                        .fontWeight(.heavy)
+                    Text("Ready To Test Your Skills?")
+                }
+                NavigationLink {
+                    QuestionView()
+                        .environmentObject(gameManager)
+                } label: {
+                    CustomButton(text: "Start")
+                }
+            }
+            .foregroundColor(.yellow)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.cyan)
         }
-        .padding()
     }
 }
 
